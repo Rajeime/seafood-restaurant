fd2e84371ecbdae4e9f67012215cb8a766be0b54
Rajeime <rajeime.m.roberts@gmail.com>
2022-04-07 15:01:28 -0500

    added stuff

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -12,4 +12,20 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  isActive: boolean = false;
+
+  showMenu(menu:any){
+    
+    this.isActive = !this.isActive;
+    if(this.isActive){
+      menu.style.display = "block" ;
+      menu.style.transition = "all 4s"
+    }
+
+    else{
+      menu.style.display = "none" ;
+      menu.style.transition = "all 4s"
+    }
+
+  }
 }
